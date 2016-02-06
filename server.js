ebfa29b60e98cfcd8fb9a0b4f063dfc0a926d88b
Shamoon Siddiqui <shamoon@develop,io>
2016-02-06 10:50:13 -0500

    Added more views

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.get("/", function(req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.get("/register", function(req, res) {
+  res.sendFile(process.cwd() + "/views/register.html");
+});
+
+app.get("/dashboard", function(req, res) {
+  res.sendFile(process.cwd() + "/views/dashboard.html");
+});
+
+
 app.listen(PORT, function() {
   console.log("Listening on port %s", PORT);
 });
